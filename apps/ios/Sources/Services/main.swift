3615acfba53bbcf0c406cd00ea47563190904266
Clifford Ray Hackett <3659745@gmail.com>
2026-02-17 11:15:42 +1000

    Delete main.swift

--- apps/ios/Sources/Services/main.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-@main
-struct HunterCLI {
-    static func main() async {
-        let service = ContactsService()
-        await service.runFullHunter(keyword: "OpenClaw")
-    }
-}
