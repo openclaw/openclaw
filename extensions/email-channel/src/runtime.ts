43a2c4cfdba73e6953c23a716a16d0922b4058dd
Gu XiaoBo <guxiaobo@example.com>
2026-02-20 23:46:20 +0800

    style: Fix formatting after merging main

--- extensions/email-channel/src/runtime.ts
+++ extensions/email-channel/src/runtime.ts
@@ -1,9 +1,9 @@
 import * as fs from "fs";
-import * as os from "os";
-import * as path from "path";
 import Imap from "imap";
 import { simpleParser } from "mailparser";
 import nodemailer from "nodemailer";
+import * as os from "os";
+import * as path from "path";
 
 interface EmailConfig {
   imap: {
