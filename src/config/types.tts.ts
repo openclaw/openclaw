daverjorge46/yoyoclaw

feat: add Typecast TTS provider types and validation schema
Add "typecast" to TtsProvider union type with provider-specific
configuration including voiceId, model (ssfm-v21/ssfm-v30), language,
emotion presets, and audio output settings.

Add corresponding Zod schema validation for the new Typecast config
block under messages.tts.typecast.

src/config/types.tts.ts
@@ -1,4 +1,4 @@
-export type TtsProvider = "elevenlabs" | "openai" | "edge";
+export type TtsProvider = "elevenlabs" | "openai" | "edge" | "typecast";
 
 export type TtsMode = "final" | "all";
 
@@ -58,6 +58,23 @@ export type TtsConfig = {
     apiKey?: string;
     model?: string;
     voice?: string;
+  };
+  /** Typecast AI configuration. */
+  typecast?: {
+    apiKey?: string;
+    baseHost?: string;
+    voiceId?: string;
+    model?: "ssfm-v21" | "ssfm-v30";
+    language?: string;
+    emotionPreset?: "normal" | "happy" | "sad" | "angry" | "whisper" | "toneup" | "tonedown";
+    emotionIntensity?: number;
+    seed?: number;
+    output?: {
+      volume?: number;
+      audioPitch?: number;
+      audioTempo?: number;
+      audioFormat?: "wav" | "mp3";
+    };
   };
   /** Microsoft Edge (node-edge-tts) configuration. */
   edge?: {

openclaw/openclaw

src/config/types.tts.ts
@@ -1,6 +1,6 @@
 import type { SecretInput } from "./types.secrets.js";
 
-export type TtsProvider = "elevenlabs" | "openai" | "edge";
+export type TtsProvider = "elevenlabs" | "openai" | "edge" | "typecast";
 
 export type TtsMode = "final" | "all";
 
@@ -61,6 +61,23 @@ export type TtsConfig = {
     baseUrl?: string;
     model?: string;
     voice?: string;
+  };
+  /** Typecast AI configuration. */
+  typecast?: {
+    apiKey?: SecretInput;
+    baseHost?: string;
+    voiceId?: string;
+    model?: "ssfm-v21" | "ssfm-v30";
+    language?: string;
+    emotionPreset?: "normal" | "happy" | "sad" | "angry" | "whisper" | "toneup" | "tonedown";
+    emotionIntensity?: number;
+    seed?: number;
+    output?: {
+      volume?: number;
+      audioPitch?: number;
+      audioTempo?: number;
+      audioFormat?: "wav" | "mp3";
+    };
   };
   /** Microsoft Edge (node-edge-tts) configuration. */
   edge?: {