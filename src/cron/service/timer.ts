baa6b5d566a41950dea0a214881eef48697326d8
Val Alexander <bunsthedev@gmail.com>
2026-03-09 23:47:05 -0500

    Cron: format timer updates

--- src/cron/service/timer.ts
+++ src/cron/service/timer.ts
@@ -1,6 +1,6 @@
+import { resolveFailoverReasonFromError } from "../../agents/failover-error.js";
 import type { CronConfig, CronRetryOn } from "../../config/types.cron.js";
 import type { HeartbeatRunResult } from "../../infra/heartbeat-wake.js";
-import { resolveFailoverReasonFromError } from "../../agents/failover-error.js";
 import { DEFAULT_AGENT_ID } from "../../routing/session-key.js";
 import { resolveCronDeliveryPlan } from "../delivery.js";
 import { sweepCronRunSessions } from "../session-reaper.js";
