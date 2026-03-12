85b4e44b73cb97750109ba26e279501cd48738eb
Tyler Yust <TYTYYUST@YAHOO.COM>
2026-03-12 10:02:25 -0700

    Add session lifecycle gateway methods

--- src/gateway/method-scopes.test.ts
+++ src/gateway/method-scopes.test.ts
@@ -11,6 +11,9 @@ describe("method scope resolution", () => {
   it.each([
     ["sessions.resolve", ["operator.read"]],
     ["config.schema.lookup", ["operator.read"]],
+    ["sessions.create", ["operator.write"]],
+    ["sessions.send", ["operator.write"]],
+    ["sessions.abort", ["operator.write"]],
     ["poll", ["operator.write"]],
     ["config.patch", ["operator.admin"]],
     ["wizard.start", ["operator.admin"]],
