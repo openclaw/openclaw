1ab70645f4ef23adfb4a4b1b2bb7881e041c397b
宇宙级编码大王 <2951020311@qq.com>
2026-03-19 12:47:25 +0800

    chore: remove temp file

--- tmp-push.js
+++
@@ -1,9 +0,0 @@
-import {execSync} from 'child_process';
-const path = 'd:/Agent/openclaw';
-try {
-  execSync('git add -A && git commit -m "chore: remove temporary script file"', {cwd: path, stdio: 'inherit'});
-  execSync('git push origin feature/tree-model-selector', {cwd: path, stdio: 'inherit'});
-} catch (e) {
-  console.error('Failed');
-  process.exit(1);
-}
