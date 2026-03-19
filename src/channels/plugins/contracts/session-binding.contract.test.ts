96184f119dc56d429ea211e72bf15de263ebf731
sunke <sunke@keep.com>
2026-03-19 21:25:27 +0800

    test: cover matrix session binding contracts

--- src/channels/plugins/contracts/session-binding.contract.test.ts
+++ src/channels/plugins/contracts/session-binding.contract.test.ts
@@ -3,13 +3,17 @@ import { __testing as discordThreadBindingTesting } from "../../../../extensions
 import { __testing as feishuThreadBindingTesting } from "../../../../extensions/feishu/src/thread-bindings.js";
 import { __testing as telegramThreadBindingTesting } from "../../../../extensions/telegram/src/thread-bindings.js";
 import { __testing as sessionBindingTesting } from "../../../infra/outbound/session-binding-service.js";
-import { sessionBindingContractRegistry } from "./registry.js";
+import {
+  resetMatrixSessionBindingContractStateForTests,
+  sessionBindingContractRegistry,
+} from "./registry.js";
 import { installSessionBindingContractSuite } from "./suites.js";
 
 beforeEach(() => {
   sessionBindingTesting.resetSessionBindingAdaptersForTests();
   discordThreadBindingTesting.resetThreadBindingsForTests();
   feishuThreadBindingTesting.resetFeishuThreadBindingsForTests();
+  resetMatrixSessionBindingContractStateForTests();
   telegramThreadBindingTesting.resetTelegramThreadBindingsForTests();
 });
 
