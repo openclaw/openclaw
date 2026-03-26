60e3d39d60bb1181a183158db3a357961a66a858
sallyom <somalley@redhat.com>
2026-03-26 18:05:40 -0400

    podman: persist runtime env defaults
    Co-authored-by: albertxos <kickban3000@gmail.com>
    Signed-off-by: sallyom <somalley@redhat.com>

--- scripts/run-openclaw-podman.sh
+++ scripts/run-openclaw-podman.sh
@@ -189,6 +189,10 @@ if [[ "$RUN_SETUP" == true ]]; then
     --init \
     "${USERNS_ARGS[@]}" "${RUN_USER_ARGS[@]}" \
     -e HOME=/home/node -e TERM=xterm-256color -e BROWSER=echo \
+    -e NPM_CONFIG_CACHE=/home/node/.openclaw/.npm \
+    -e NPM_CONFIG_PREFIX=/home/node/.openclaw/.npm-global \
+    -e PATH=/home/node/.openclaw/.npm-global/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin \
+    -e OPENCLAW_NO_RESPAWN=1 \
     -e OPENCLAW_GATEWAY_TOKEN="$OPENCLAW_GATEWAY_TOKEN" \
     -v "$CONFIG_DIR:/home/node/.openclaw:rw${SELINUX_MOUNT_OPTS}" \
     -v "$WORKSPACE_DIR:/home/node/.openclaw/workspace:rw${SELINUX_MOUNT_OPTS}" \
@@ -202,6 +206,10 @@ podman run --pull="$PODMAN_PULL" -d --replace \
   --init \
   "${USERNS_ARGS[@]}" "${RUN_USER_ARGS[@]}" \
   -e HOME=/home/node -e TERM=xterm-256color \
+  -e NPM_CONFIG_CACHE=/home/node/.openclaw/.npm \
+  -e NPM_CONFIG_PREFIX=/home/node/.openclaw/.npm-global \
+  -e PATH=/home/node/.openclaw/.npm-global/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin \
+  -e OPENCLAW_NO_RESPAWN=1 \
   -e OPENCLAW_GATEWAY_TOKEN="$OPENCLAW_GATEWAY_TOKEN" \
   "${ENV_FILE_ARGS[@]}" \
   -v "$CONFIG_DIR:/home/node/.openclaw:rw${SELINUX_MOUNT_OPTS}" \
