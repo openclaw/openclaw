da2fbee19359d8e8d33f97cdf4bf2add3996f03c
Pavan Kumar Gondhi <pgondhi@nvidia.com>
2026-03-31 08:31:02 +0000

    fix: address build errors

---
+++ extensions/qqbot/src/silk-wasm.d.ts
@@ -0,0 +1,10 @@
+declare module "silk-wasm" {
+  export interface SilkCodecResult {
+    data: Uint8Array;
+    duration: number;
+  }
+
+  export function isSilk(input: Uint8Array): boolean;
+  export function decode(input: Uint8Array, sampleRate: number): Promise<SilkCodecResult>;
+  export function encode(input: Uint8Array, sampleRate: number): Promise<SilkCodecResult>;
+}
