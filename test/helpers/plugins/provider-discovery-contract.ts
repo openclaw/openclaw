a30d592e93a356802ce029d2bc1f21c4bf9379a4
Dharit Shah <dhshah@redhat.com>
2026-04-04 23:38:05 -0400

    Fix contract protocol CI regressions

--- test/helpers/plugins/provider-discovery-contract.ts
+++ test/helpers/plugins/provider-discovery-contract.ts
@@ -205,11 +205,9 @@ function installDiscoveryHooks(
       };
     });
     vi.doMock(bundledProviderModules.vllmApiModuleId, async () => {
+      const actual = await vi.importActual<object>(bundledProviderModules.vllmApiModuleId);
       return {
-        VLLM_DEFAULT_API_KEY_ENV_VAR: "VLLM_API_KEY",
-        VLLM_DEFAULT_BASE_URL: "http://127.0.0.1:8000/v1",
-        VLLM_MODEL_PLACEHOLDER: "meta-llama/Meta-Llama-3-8B-Instruct",
-        VLLM_PROVIDER_LABEL: "vLLM",
+        ...actual,
         buildVllmProvider: (...args: unknown[]) => buildVllmProviderMock(...args),
       };
     });
