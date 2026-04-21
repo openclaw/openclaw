Arry8/openclaw-edge

fix(slack): preserve direct send thread aliases

src/cli/send-runtime/channel-outbound-send.test.ts
@@ -1,0 +1,85 @@
+import { beforeEach, describe, expect, it, vi } from "vitest";
+
+const mocks = vi.hoisted(() => ({
+  loadChannelOutboundAdapter: vi.fn(),
+  loadConfig: vi.fn(() => ({ test: true })),
+  sendText: vi.fn(async () => ({ messageId: "m1" })),
+}));
+
+vi.mock("../../channels/plugins/outbound/load.js", () => ({
+  loadChannelOutboundAdapter: mocks.loadChannelOutboundAdapter,
+}));
+
+vi.mock("../../config/config.js", () => ({
+  loadConfig: mocks.loadConfig,
+}));
+
+import { createChannelOutboundRuntimeSend } from "./channel-outbound-send.js";
+
+describe("createChannelOutboundRuntimeSend", () => {
+  beforeEach(() => {
+    vi.clearAllMocks();
+    mocks.loadChannelOutboundAdapter.mockResolvedValue({
+      sendText: mocks.sendText,
+    });
+  });
+
+  it("forwards threadTs alias to threadId", async () => {
+    const runtimeSend = createChannelOutboundRuntimeSend({
+      channelId: "slack",
+      unavailableMessage: "missing",
+    });
+
+    await runtimeSend.sendMessage("C123", "hello", {
+      threadTs: "1712345678.123456",
+    });
+
+    expect(mocks.sendText).toHaveBeenCalledWith(
+      expect.objectContaining({
+        to: "C123",
+        text: "hello",
+        threadId: "1712345678.123456",
+      }),
+    );
+  });
+
+  it("forwards replyToId alias to replyToId", async () => {
+    const runtimeSend = createChannelOutboundRuntimeSend({
+      channelId: "slack",
+      unavailableMessage: "missing",
+    });
+
+    await runtimeSend.sendMessage("C123", "hello", {
+      replyToId: "1712000000.000001",
+    });
+
+    expect(mocks.sendText).toHaveBeenCalledWith(
+      expect.objectContaining({
+        to: "C123",
+        text: "hello",
+        replyToId: "1712000000.000001",
+      }),
+    );
+  });
+
+  it("prefers canonical fields over aliases", async () => {
+    const runtimeSend = createChannelOutboundRuntimeSend({
+      channelId: "slack",
+      unavailableMessage: "missing",
+    });
+
+    await runtimeSend.sendMessage("C123", "hello", {
+      messageThreadId: "200.000",
+      threadTs: "100.000",
+      replyToMessageId: "400.000",
+      replyToId: "300.000",
+    });
+
+    expect(mocks.sendText).toHaveBeenCalledWith(
+      expect.objectContaining({
+        threadId: "200.000",
+        replyToId: "400.000",
+      }),
+    );
+  });
+});

openclaw/openclaw

src/cli/send-runtime/channel-outbound-send.test.ts
@@ -115,6 +115,63 @@ describe("createChannelOutboundRuntimeSend", () => {
     );
   });
 
+  it("forwards Slack threadTs alias to threadId", async () => {
+    const sendText = vi.fn(async () => ({ channel: "slack", messageId: "slack-1" }));
+    mocks.loadChannelOutboundAdapter.mockResolvedValue({
+      sendText,
+    });
+
+    const { createChannelOutboundRuntimeSend } = await import("./channel-outbound-send.js");
+    const runtimeSend = createChannelOutboundRuntimeSend({
+      channelId: "slack" as never,
+      unavailableMessage: "unavailable",
+    });
+
+    await runtimeSend.sendMessage("C123", "hello", {
+      cfg: {},
+      threadTs: "1712345678.123456",
+    });
+
+    expect(sendText).toHaveBeenCalledWith(
+      expect.objectContaining({
+        cfg: {},
+        to: "C123",
+        text: "hello",
+        threadId: "1712345678.123456",
+      }),
+    );
+  });
+
+  it("prefers canonical thread fields over Slack aliases", async () => {
+    const sendText = vi.fn(async () => ({ channel: "slack", messageId: "slack-2" }));
+    mocks.loadChannelOutboundAdapter.mockResolvedValue({
+      sendText,
+    });
+
+    const { createChannelOutboundRuntimeSend } = await import("./channel-outbound-send.js");
+    const runtimeSend = createChannelOutboundRuntimeSend({
+      channelId: "slack" as never,
+      unavailableMessage: "unavailable",
+    });
+
+    await runtimeSend.sendMessage("C123", "hello", {
+      cfg: {},
+      messageThreadId: "200.000",
+      threadId: "150.000",
+      threadTs: "100.000",
+      replyToMessageId: "400.000",
+      replyToId: "300.000",
+    });
+
+    expect(sendText).toHaveBeenCalledWith(
+      expect.objectContaining({
+        cfg: {},
+        threadId: "200.000",
+        replyToId: "400.000",
+      }),
+    );
+  });
+
   it("falls back to sendText when media is present but sendMedia is unavailable", async () => {
     const sendText = vi.fn(async () => ({ channel: "whatsapp", messageId: "wa-3" }));
     mocks.loadChannelOutboundAdapter.mockResolvedValue({