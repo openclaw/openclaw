3af85b7eea9dc48ce45a9da8aceef8e2d38f1185
Eva <eva@100yen.org>
2026-04-25 04:47:35 +0700

    fix: satisfy runtime plan compat lint

--- src/agents/runtime-plan/types.compat.test.ts
+++ src/agents/runtime-plan/types.compat.test.ts
@@ -10,12 +10,7 @@ import type {
   AgentRuntimeThinkLevel,
 } from "./types.js";
 
-type Equal<X, Y> =
-  (<T>() => T extends X ? 1 : 2) extends <T>() => T extends Y ? 1 : 2
-    ? (<T>() => T extends Y ? 1 : 2) extends <T>() => T extends X ? 1 : 2
-      ? true
-      : false
-    : false;
+type Equal<X, Y> = [X] extends [Y] ? ([Y] extends [X] ? true : false) : false;
 
 type Assert<T extends true> = T;
 
