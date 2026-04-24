691cd3de1cc6b487d3bf6135eb51a1bd40ef4fe7
Eva <eva@100yen.org>
2026-04-25 04:02:18 +0700

    refactor: delegate embedded runner directory barrel

--- src/agents/embedded-runner/index.ts
+++ src/agents/embedded-runner/index.ts
@@ -1,17 +1,1 @@
-export {
-  abortEmbeddedAgentRun,
-  compactEmbeddedAgentSession,
-  isEmbeddedAgentRunActive,
-  isEmbeddedAgentRunStreaming,
-  queueEmbeddedAgentMessage,
-  resolveActiveEmbeddedAgentRunSessionId,
-  resolveEmbeddedSessionLane,
-  runEmbeddedAgent,
-  waitForEmbeddedAgentRunEnd,
-} from "../embedded-runner.js";
-export type {
-  EmbeddedAgentCompactResult,
-  EmbeddedAgentMeta,
-  EmbeddedAgentRunMeta,
-  EmbeddedAgentRunResult,
-} from "../embedded-runner.js";
+export * from "../embedded-runner.js";
