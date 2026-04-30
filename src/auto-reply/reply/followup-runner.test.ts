artiemellors/Claw-Test

fix: dispatch remaining cli-backed runs

src/auto-reply/reply/followup-runner.test.ts
@@ -6,6 +6,7 @@ import type { SessionEntry } from "../../config/sessions/types.js";
 import type { FollowupRun, QueueSettings } from "./queue.js";
 
 const runEmbeddedPiAgentMock = vi.fn();
+const runCliAgentMock = vi.fn();
 const compactEmbeddedPiSessionMock = vi.fn();
 const routeReplyMock = vi.fn();
 const isRoutableChannelMock = vi.fn();
@@ -243,6 +244,9 @@ vi.mock("../../agents/pi-embedded.js", () => ({
   runEmbeddedPiAgent: (params: unknown) => runEmbeddedPiAgentMock(params),
   waitForEmbeddedPiRunEnd: vi.fn(async () => undefined),
 }));
+vi.mock("../../agents/cli-runner.js", () => ({
+  runCliAgent: (params: unknown) => runCliAgentMock(params),
+}));
 vi.mock("./queue.js", () => ({
   clearFollowupQueue: clearFollowupQueueForFollowupTest,
   enqueueFollowupRun: enqueueFollowupRunForFollowupTest,
@@ -280,6 +284,7 @@ const ROUTABLE_TEST_CHANNELS = new Set([
 
 beforeEach(async () => {
   runEmbeddedPiAgentMock.mockReset();
+  runCliAgentMock.mockReset();
   compactEmbeddedPiSessionMock.mockReset();
   runPreflightCompactionIfNeededMock.mockReset();
   runPreflightCompactionIfNeededMock.mockImplementation(
@@ -706,6 +711,50 @@ describe("createFollowupRunner compaction", () => {
     expect(embeddedCalls[0]?.extraSystemPrompt).toContain("Post-compaction context refresh");
     expect(embeddedCalls[0]?.extraSystemPrompt).toContain("Read AGENTS.md before replying.");
     expect(sessionStore.main?.compactionCount).toBe(2);
+  });
+});
+
+describe("createFollowupRunner CLI backend dispatch", () => {
+  it("routes CLI-backed followups through runCliAgent", async () => {
+    const onBlockReply = vi.fn(async () => {});
+    runCliAgentMock.mockResolvedValueOnce({
+      payloads: [{ text: "cli reply" }],
+      meta: {
+        agentMeta: {
+          sessionId: "cli-session",
+          provider: "claude-cli",
+          model: "opus",
+          cliSessionBinding: { sessionId: "cli-session" },
+        },
+      },
+    });
+
+    const runner = createFollowupRunner({
+      opts: { onBlockReply },
+      typing: createMockTypingController(),
+      typingMode: "instant",
+      sessionKey: "main",
+      defaultModel: "claude-cli/opus",
+    });
+
+    const queued = createQueuedRun({
+      run: {
+        provider: "claude-cli",
+        model: "opus",
+      },
+    });
+
+    await runner(queued);
+
+    expect(runCliAgentMock).toHaveBeenCalledWith(
+      expect.objectContaining({
+        provider: "claude-cli",
+        model: "opus",
+        prompt: queued.prompt,
+      }),
+    );
+    expect(runEmbeddedPiAgentMock).not.toHaveBeenCalled();
+    expect(onBlockReply).toHaveBeenCalled();
   });
 });
 

src/hooks/llm-slug-generator.ts
@@ -11,8 +11,9 @@ import {
   resolveAgentDir,
   resolveAgentEffectiveModelPrimary,
 } from "../agents/agent-scope.js";
+import { runCliAgent } from "../agents/cli-runner.js";
 import { DEFAULT_PROVIDER, DEFAULT_MODEL } from "../agents/defaults.js";
-import { parseModelRef } from "../agents/model-selection.js";
+import { isCliProvider, parseModelRef } from "../agents/model-selection.js";
 import { runEmbeddedPiAgent } from "../agents/pi-embedded.js";
 import type { OpenClawConfig } from "../config/config.js";
 import { createSubsystemLogger } from "../logging/subsystem.js";
@@ -50,20 +51,36 @@ Reply with ONLY the slug, nothing else. Examples: "vendor-pitch", "api-design", 
     const provider = parsed?.provider ?? DEFAULT_PROVIDER;
     const model = parsed?.model ?? DEFAULT_MODEL;
 
-    const result = await runEmbeddedPiAgent({
-      sessionId: `slug-generator-${Date.now()}`,
-      sessionKey: "temp:slug-generator",
-      agentId,
-      sessionFile: tempSessionFile,
-      workspaceDir,
-      agentDir,
-      config: params.cfg,
-      prompt,
-      provider,
-      model,
-      timeoutMs: 15_000, // 15 second timeout
-      runId: `slug-gen-${Date.now()}`,
-    });
+    const sessionId = `slug-generator-${Date.now()}`;
+    const runId = `slug-gen-${Date.now()}`;
+    const result = isCliProvider(provider, params.cfg)
+      ? await runCliAgent({
+          sessionId,
+          sessionKey: "temp:slug-generator",
+          agentId,
+          sessionFile: tempSessionFile,
+          workspaceDir,
+          config: params.cfg,
+          prompt,
+          provider,
+          model,
+          timeoutMs: 15_000,
+          runId,
+        })
+      : await runEmbeddedPiAgent({
+          sessionId,
+          sessionKey: "temp:slug-generator",
+          agentId,
+          sessionFile: tempSessionFile,
+          workspaceDir,
+          agentDir,
+          config: params.cfg,
+          prompt,
+          provider,
+          model,
+          timeoutMs: 15_000,
+          runId,
+        });
 
     // Extract text from payloads
     if (result.payloads && result.payloads.length > 0) {

openclaw/openclaw

src/auto-reply/reply/followup-runner.test.ts
@@ -8,6 +8,7 @@ import type { SessionEntry } from "../../config/sessions/types.js";
 import type { FollowupRun, QueueSettings } from "./queue.js";
 
 const runEmbeddedPiAgentMock = vi.fn();
+const runCliAgentMock = vi.fn();
 const compactEmbeddedPiSessionMock = vi.fn();
 const routeReplyMock = vi.fn();
 const isRoutableChannelMock = vi.fn();
@@ -265,6 +266,9 @@ async function loadFreshFollowupRunnerModuleForTest() {
     runEmbeddedPiAgent: (params: unknown) => runEmbeddedPiAgentMock(params),
     waitForEmbeddedPiRunEnd: vi.fn(async () => undefined),
   }));
+  vi.doMock("../../agents/cli-runner.js", () => ({
+    runCliAgent: (params: unknown) => runCliAgentMock(params),
+  }));
   vi.doMock("./queue.js", () => ({
     clearFollowupQueue: clearFollowupQueueForFollowupTest,
     enqueueFollowupRun: enqueueFollowupRunForFollowupTest,
@@ -366,6 +370,7 @@ beforeAll(async () => {
 beforeEach(() => {
   clearRuntimeConfigSnapshot?.();
   runEmbeddedPiAgentMock.mockReset();
+  runCliAgentMock.mockReset();
   compactEmbeddedPiSessionMock.mockReset();
   runPreflightCompactionIfNeededMock.mockReset();
   resolveCommandSecretRefsViaGatewayMock.mockReset();
@@ -1000,6 +1005,50 @@ describe("createFollowupRunner compaction", () => {
     expect(embeddedCalls[0]?.extraSystemPrompt).toContain("Post-compaction context refresh");
     expect(embeddedCalls[0]?.extraSystemPrompt).toContain("Read AGENTS.md before replying.");
     expect(sessionStore.main?.compactionCount).toBe(2);
+  });
+});
+
+describe("createFollowupRunner CLI backend dispatch", () => {
+  it("routes CLI-backed followups through runCliAgent", async () => {
+    const onBlockReply = vi.fn(async () => {});
+    runCliAgentMock.mockResolvedValueOnce({
+      payloads: [{ text: "cli reply" }],
+      meta: {
+        agentMeta: {
+          sessionId: "cli-session",
+          provider: "claude-cli",
+          model: "opus",
+          cliSessionBinding: { sessionId: "cli-session" },
+        },
+      },
+    });
+
+    const runner = createFollowupRunner({
+      opts: { onBlockReply },
+      typing: createMockTypingController(),
+      typingMode: "instant",
+      sessionKey: "main",
+      defaultModel: "claude-cli/opus",
+    });
+
+    const queued = createQueuedRun({
+      run: {
+        provider: "claude-cli",
+        model: "opus",
+      },
+    });
+
+    await runner(queued);
+
+    expect(runCliAgentMock).toHaveBeenCalledWith(
+      expect.objectContaining({
+        provider: "claude-cli",
+        model: "opus",
+        prompt: queued.prompt,
+      }),
+    );
+    expect(runEmbeddedPiAgentMock).not.toHaveBeenCalled();
+    expect(onBlockReply).toHaveBeenCalled();
   });
 });
 

src/hooks/llm-slug-generator.ts
@@ -10,7 +10,9 @@ import {
   resolveAgentWorkspaceDir,
   resolveAgentDir,
 } from "../agents/agent-scope.js";
-import { resolveDefaultModelForAgent } from "../agents/model-selection.js";
+import { runCliAgent } from "../agents/cli-runner.js";
+import { isCliProvider, resolveDefaultModelForAgent } from "../agents/model-selection.js";
+import { resolveCliRuntimeExecutionProvider } from "../agents/model-runtime-aliases.js";
 import { runEmbeddedPiAgent } from "../agents/pi-embedded.js";
 import { resolveAgentTimeoutMs } from "../agents/timeout.js";
 import type { OpenClawConfig } from "../config/types.openclaw.js";
@@ -59,21 +61,43 @@ Reply with ONLY the slug, nothing else. Examples: "vendor-pitch", "api-design", 
     });
     const timeoutMs = resolveSlugGeneratorTimeoutMs(params.cfg);
 
-    const result = await runEmbeddedPiAgent({
-      sessionId: `slug-generator-${Date.now()}`,
-      sessionKey: "temp:slug-generator",
-      agentId,
-      sessionFile: tempSessionFile,
-      workspaceDir,
-      agentDir,
-      config: params.cfg,
-      prompt,
-      provider,
-      model,
-      timeoutMs,
-      runId: `slug-gen-${Date.now()}`,
-      cleanupBundleMcpOnRunEnd: true,
-    });
+    const sessionId = `slug-generator-${Date.now()}`;
+    const runId = `slug-gen-${Date.now()}`;
+    const cliExecutionProvider =
+      resolveCliRuntimeExecutionProvider({
+        provider,
+        cfg: params.cfg,
+        agentId,
+      }) ?? provider;
+    const result = isCliProvider(cliExecutionProvider, params.cfg)
+      ? await runCliAgent({
+          sessionId,
+          sessionKey: "temp:slug-generator",
+          agentId,
+          sessionFile: tempSessionFile,
+          workspaceDir,
+          config: params.cfg,
+          prompt,
+          provider: cliExecutionProvider,
+          model,
+          timeoutMs,
+          runId,
+        })
+      : await runEmbeddedPiAgent({
+          sessionId,
+          sessionKey: "temp:slug-generator",
+          agentId,
+          sessionFile: tempSessionFile,
+          workspaceDir,
+          agentDir,
+          config: params.cfg,
+          prompt,
+          provider,
+          model,
+          timeoutMs,
+          runId,
+          cleanupBundleMcpOnRunEnd: true,
+        });
 
     // Extract text from payloads
     if (result.payloads && result.payloads.length > 0) {