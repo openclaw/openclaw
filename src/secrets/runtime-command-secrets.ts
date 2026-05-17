f27ed3f7ce6fdd4e3ce7f36b8391ef8b95bad019
Peter Steinberger <steipete@gmail.com>
2026-05-17 10:50:32 +0100

    style(secrets): format runtime command imports

--- src/secrets/runtime-command-secrets.ts
+++ src/secrets/runtime-command-secrets.ts
@@ -10,11 +10,8 @@ import {
 } from "./command-config.js";
 import { getPath, setPathExistingStrict } from "./path-utils.js";
 import { resolveSecretRefValue } from "./resolve.js";
-import {
-  getActiveSecretsRuntimeEnv,
-  getActiveSecretsRuntimeSnapshot,
-} from "./runtime-state.js";
 import { createResolverContext } from "./runtime-shared.js";
+import { getActiveSecretsRuntimeEnv, getActiveSecretsRuntimeSnapshot } from "./runtime-state.js";
 import { resolveRuntimeWebTools } from "./runtime-web-tools.js";
 import { assertExpectedResolvedSecretValue } from "./secret-value.js";
 import { discoverConfigSecretTargetsByIds } from "./target-registry.js";
