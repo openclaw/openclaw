f78d0e22d9372193b61c5253c487299e8bc3fd62
Peter Steinberger <steipete@gmail.com>
2026-05-17 02:44:12 +0100

    test: expose plugin node mock helpers

--- src/plugin-sdk/test-node-mocks.ts
+++ src/plugin-sdk/test-node-mocks.ts
@@ -5,3 +5,9 @@ export {
   mockNodeChildProcessExecFile,
   mockNodeChildProcessSpawnSync,
 } from "./test-helpers/node-builtin-mocks.js";
+export {
+  withMockedPlatform,
+  withMockedWindowsPlatform,
+  withRestoredMocks,
+  type RestorableMock,
+} from "../test-utils/vitest-spies.js";
