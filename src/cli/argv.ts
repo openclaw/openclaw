4b4be8ae2ad8b9cf728ee267272d576fba092197
d3lap1ace <reald3lap1ace@gmail.com>
2026-05-23 20:04:58 +0800

    fix: use FLAG_TERMINATOR constant in getCommandPathInternal

--- src/cli/argv.ts
+++ src/cli/argv.ts
@@ -222,7 +222,7 @@ function getCommandPathInternal(
     if (!arg) {
       continue;
     }
-    if (arg === "--") {
+    if (arg === FLAG_TERMINATOR) {
       break;
     }
     if (opts.skipRootOptions) {
