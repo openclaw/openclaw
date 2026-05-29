auraecosystem/Openclaw-builder

Fix replay-invalid session recovery

src/auto-reply/reply/agent-runner-execution.test.ts
@@ -79,6 +79,12 @@ vi.mock("../../agents/bootstrap-budget.js", () => ({
 
 vi.mock("../../agents/pi-embedded-helpers.js", () => ({
   BILLING_ERROR_USER_MESSAGE: "billing",
+  classifyProviderRuntimeFailureKind: (message: string) =>
+    /invalid_encrypted_content|encrypted content could not be decrypted or parsed|tool_(?:use|call)\.(?:input|arguments)|input item id does not belong to this connection/i.test(
+      message,
+    )
+      ? "replay_invalid"
+      : "unclassified",
   formatRateLimitOrOverloadedErrorCopy: (message: string) => {
     if (/model\s+(?:is\s+)?at capacity/i.test(message)) {
       return "⚠️ Selected model is at capacity. Try a different model, or wait and retry.";
@@ -4745,6 +4751,50 @@ describe("runAgentTurnWithFallback", () => {
     expect(result.kind).toBe("final");
     if (result.kind === "final") {
       expect(result.payload.text).toBe(PROVIDER_CONVERSATION_STATE_ERROR_USER_MESSAGE);
+    }
+  });
+
+  it("returns a session reset hint for OpenAI encrypted replay failures", async () => {
+    const resetSessionAfterReplayInvalid = vi.fn(async () => true);
+    state.runEmbeddedPiAgentMock.mockRejectedValueOnce(
+      new Error(
+        "400 code=invalid_encrypted_content Encrypted content could not be decrypted or parsed.",
+      ),
+    );
+
+    const runAgentTurnWithFallback = await getRunAgentTurnWithFallback();
+    const result = await runAgentTurnWithFallback({
+      commandBody: "hello",
+      followupRun: createFollowupRun(),
+      sessionCtx: {
+        Provider: "telegram",
+        MessageSid: "msg",
+      } as unknown as TemplateContext,
+      opts: {},
+      typingSignals: createMockTypingSignaler(),
+      blockReplyPipeline: null,
+      blockStreamingEnabled: false,
+      resolvedBlockStreamingBreak: "message_end",
+      applyReplyToMode: (payload) => payload,
+      shouldEmitToolResult: () => true,
+      shouldEmitToolOutput: () => false,
+      pendingToolTasks: new Set(),
+      resetSessionAfterRoleOrderingConflict: async () => false,
+      resetSessionAfterReplayInvalid,
+      isHeartbeat: false,
+      sessionKey: "main",
+      getActiveSessionEntry: () => undefined,
+      resolvedVerboseLevel: "off",
+    });
+
+    expect(resetSessionAfterReplayInvalid).toHaveBeenCalledWith(
+      "400 code=invalid_encrypted_content Encrypted content could not be decrypted or parsed.",
+    );
+    expect(result.kind).toBe("final");
+    if (result.kind === "final") {
+      expect(result.payload.text).toBe(
+        "⚠️ Session history got out of sync. Please try again, or use /new to start a fresh session.",
+      );
     }
   });
 

src/auto-reply/reply/agent-runner.runreplyagent.e2e.test.ts
@@ -196,6 +196,7 @@ function createMinimalRun(params?: {
   return {
     typing,
     opts,
+    followupRun,
     run: async () => {
       const runReplyAgent = await getRunReplyAgent();
       return runReplyAgent({
@@ -349,6 +350,61 @@ describe("runReplyAgent pending final delivery capture", () => {
     const raw = await readFile(storePath, "utf8");
     return JSON.parse(raw).main as SessionEntry;
   }
+
+  it("rotates poisoned replay-invalid sessions before returning reset guidance", async () => {
+    const sessionEntry: SessionEntry = {
+      sessionId: "poisoned-session",
+      sessionFile: "/tmp/poisoned-session.jsonl",
+      updatedAt: Date.now(),
+      sessionStartedAt: Date.now() - 10_000,
+      systemSent: true,
+      status: "running",
+      contextTokens: 272_000,
+      inputTokens: 877_883,
+      outputTokens: 5_500,
+      modelProvider: "openai-codex",
+      model: "gpt-5.5",
+      cliSessionBindings: { "codex-cli": { sessionId: "codex-session" } },
+    };
+    const sessionStore = { main: sessionEntry };
+    const storePath = await createSessionStoreFile(sessionEntry);
+    state.runEmbeddedPiAgentMock.mockRejectedValueOnce(
+      new Error(
+        "400 code=invalid_encrypted_content Encrypted content could not be decrypted or parsed.",
+      ),
+    );
+
+    const { followupRun, run } = createMinimalRun({
+      sessionEntry,
+      sessionStore,
+      sessionKey: "main",
+      storePath,
+      runOverrides: {
+        provider: "openai-codex",
+        model: "gpt-5.5",
+      },
+    });
+
+    const result = await run();
+    const stored = await readStoredMainSession(storePath);
+
+    expect(result).toMatchObject({
+      text: "⚠️ Session history got out of sync. Please try again, or use /new to start a fresh session.",
+    });
+    expect(stored.sessionId).not.toBe("poisoned-session");
+    expect(stored.sessionFile).toEqual(expect.any(String));
+    expect(stored.sessionFile).not.toBe("/tmp/poisoned-session.jsonl");
+    expect(followupRun.run.sessionId).toBe(stored.sessionId);
+    expect(followupRun.run.sessionFile).toBe(stored.sessionFile);
+    expect(vi.mocked(refreshQueuedFollowupSession)).toHaveBeenCalledWith({
+      key: "main",
+      previousSessionId: "poisoned-session",
+      nextSessionId: stored.sessionId,
+      nextSessionFile: stored.sessionFile,
+    });
+    expect(stored.systemSent).toBe(false);
+    expect(stored.contextTokens).toBeUndefined();
+  });
 
   it("does not persist message-tool-only final replies for heartbeat replay", async () => {
     const sessionEntry: SessionEntry = {

openclaw/openclaw

src/auto-reply/reply/agent-runner-execution.test.ts
@@ -96,6 +96,12 @@ vi.mock("../../agents/bootstrap-budget.js", () => ({
 
 vi.mock("../../agents/embedded-agent-helpers.js", () => ({
   BILLING_ERROR_USER_MESSAGE: "billing",
+  classifyProviderRuntimeFailureKind: (message: string) =>
+    /invalid_encrypted_content|encrypted content could not be decrypted or parsed|tool_(?:use|call)\.(?:input|arguments)|input item id does not belong to this connection/i.test(
+      message,
+    )
+      ? "replay_invalid"
+      : "unclassified",
   formatRateLimitOrOverloadedErrorCopy: (message: string) => {
     if (/model\s+(?:is\s+)?at capacity/i.test(message)) {
       return "⚠️ Selected model is at capacity. Try a different model, or wait and retry.";
@@ -5473,6 +5479,50 @@ describe("runAgentTurnWithFallback", () => {
     expect(result.kind).toBe("final");
     if (result.kind === "final") {
       expect(result.payload.text).toBe(PROVIDER_CONVERSATION_STATE_ERROR_USER_MESSAGE);
+    }
+  });
+
+  it("returns a session reset hint for OpenAI encrypted replay failures", async () => {
+    const resetSessionAfterReplayInvalid = vi.fn(async () => true);
+    state.runEmbeddedAgentMock.mockRejectedValueOnce(
+      new Error(
+        "400 code=invalid_encrypted_content Encrypted content could not be decrypted or parsed.",
+      ),
+    );
+
+    const runAgentTurnWithFallback = await getRunAgentTurnWithFallback();
+    const result = await runAgentTurnWithFallback({
+      commandBody: "hello",
+      followupRun: createFollowupRun(),
+      sessionCtx: {
+        Provider: "telegram",
+        MessageSid: "msg",
+      } as unknown as TemplateContext,
+      opts: {},
+      typingSignals: createMockTypingSignaler(),
+      blockReplyPipeline: null,
+      blockStreamingEnabled: false,
+      resolvedBlockStreamingBreak: "message_end",
+      applyReplyToMode: (payload) => payload,
+      shouldEmitToolResult: () => true,
+      shouldEmitToolOutput: () => false,
+      pendingToolTasks: new Set(),
+      resetSessionAfterRoleOrderingConflict: async () => false,
+      resetSessionAfterReplayInvalid,
+      isHeartbeat: false,
+      sessionKey: "main",
+      getActiveSessionEntry: () => undefined,
+      resolvedVerboseLevel: "off",
+    });
+
+    expect(resetSessionAfterReplayInvalid).toHaveBeenCalledWith(
+      "400 code=invalid_encrypted_content Encrypted content could not be decrypted or parsed.",
+    );
+    expect(result.kind).toBe("final");
+    if (result.kind === "final") {
+      expect(result.payload.text).toBe(
+        "⚠️ Session history got out of sync. Please try again, or use /new to start a fresh session.",
+      );
     }
   });
 

src/auto-reply/reply/agent-runner.runreplyagent.e2e.test.ts
@@ -200,6 +200,7 @@ function createMinimalRun(params?: {
   return {
     typing,
     opts,
+    followupRun,
     run: async () => {
       const runReplyAgent = await getRunReplyAgent();
       return runReplyAgent({
@@ -425,6 +426,61 @@ describe("runReplyAgent pending final delivery capture", () => {
     const raw = await readFile(storePath, "utf8");
     return JSON.parse(raw).main as SessionEntry;
   }
+
+  it("rotates poisoned replay-invalid sessions before returning reset guidance", async () => {
+    const sessionEntry: SessionEntry = {
+      sessionId: "poisoned-session",
+      sessionFile: "/tmp/poisoned-session.jsonl",
+      updatedAt: Date.now(),
+      sessionStartedAt: Date.now() - 10_000,
+      systemSent: true,
+      status: "running",
+      contextTokens: 272_000,
+      inputTokens: 877_883,
+      outputTokens: 5_500,
+      modelProvider: "openai-codex",
+      model: "gpt-5.5",
+      cliSessionBindings: { "codex-cli": { sessionId: "codex-session" } },
+    };
+    const sessionStore = { main: sessionEntry };
+    const storePath = await createSessionStoreFile(sessionEntry);
+    state.runEmbeddedAgentMock.mockRejectedValueOnce(
+      new Error(
+        "400 code=invalid_encrypted_content Encrypted content could not be decrypted or parsed.",
+      ),
+    );
+
+    const { followupRun, run } = createMinimalRun({
+      sessionEntry,
+      sessionStore,
+      sessionKey: "main",
+      storePath,
+      runOverrides: {
+        provider: "openai-codex",
+        model: "gpt-5.5",
+      },
+    });
+
+    const result = await run();
+    const stored = await readStoredMainSession(storePath);
+
+    expect(result).toMatchObject({
+      text: "⚠️ Session history got out of sync. Please try again, or use /new to start a fresh session.",
+    });
+    expect(stored.sessionId).not.toBe("poisoned-session");
+    expect(stored.sessionFile).toEqual(expect.any(String));
+    expect(stored.sessionFile).not.toBe("/tmp/poisoned-session.jsonl");
+    expect(followupRun.run.sessionId).toBe(stored.sessionId);
+    expect(followupRun.run.sessionFile).toBe(stored.sessionFile);
+    expect(vi.mocked(refreshQueuedFollowupSession)).toHaveBeenCalledWith({
+      key: "main",
+      previousSessionId: "poisoned-session",
+      nextSessionId: stored.sessionId,
+      nextSessionFile: stored.sessionFile,
+    });
+    expect(stored.systemSent).toBe(false);
+    expect(stored.contextTokens).toBeUndefined();
+  });
 
   it("does not persist message-tool-only final replies for heartbeat replay", async () => {
     const sessionEntry: SessionEntry = {