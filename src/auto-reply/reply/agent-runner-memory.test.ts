auraecosystem/Openclaw-builder

fix: complete compaction override propagation

src/auto-reply/reply/agent-runner-memory.test.ts
@@ -74,6 +74,7 @@ type EmbeddedPiAgentParams = {
 type CompactEmbeddedPiSessionParams = {
   sessionKey?: string;
   sandboxSessionKey?: string;
+  agentId?: string;
   currentTokenCount?: number;
   sessionFile?: string;
   sessionId?: string;
@@ -697,6 +698,53 @@ describe("runMemoryFlushIfNeeded", () => {
     const compactCall = requireCompactEmbeddedPiSessionCall();
     expect(compactCall.sessionKey).toBe("agent:main:main");
     expect(compactCall.sandboxSessionKey).toBe("agent:main:telegram:default:direct:12345");
+  });
+
+  it("uses inherited reserveTokensFloor for Codex-runtime preflight projection with empty agent compaction", async () => {
+    clearMemoryPluginState();
+    const sessionEntry: SessionEntry = {
+      sessionId: "session",
+      updatedAt: Date.now(),
+      totalTokens: 5_000,
+      totalTokensFresh: false,
+    };
+
+    await runPreflightCompactionIfNeeded({
+      cfg: {
+        agents: {
+          defaults: {
+            model: "openai/gpt-5.5",
+            compaction: { reserveTokensFloor: 3_000 },
+          },
+          list: [
+            {
+              id: "codex-worker",
+              model: { primary: "openai/gpt-5.5" },
+              models: { "openai/gpt-5.5": { agentRuntime: { id: "codex" } } },
+              compaction: {},
+            },
+          ],
+        },
+      },
+      followupRun: createTestFollowupRun({
+        agentId: "codex-worker",
+        provider: "openai",
+        model: "gpt-5.5",
+        sessionKey: "agent:codex-worker:main",
+      }),
+      defaultModel: "gpt-5.5",
+      agentCfgContextTokens: 10_000,
+      sessionEntry,
+      sessionStore: { "agent:codex-worker:main": sessionEntry },
+      sessionKey: "agent:codex-worker:main",
+      isHeartbeat: false,
+      replyOperation: createReplyOperation(),
+    });
+
+    expect(compactEmbeddedPiSessionMock).toHaveBeenCalledTimes(1);
+    const compactCall = requireCompactEmbeddedPiSessionCall();
+    expect(compactCall.agentId).toBe("codex-worker");
+    expect(compactCall.currentTokenCount).toBe(5_000);
   });
 
   it("updates the active preflight run after transcript rotation", async () => {

src/auto-reply/reply/agent-runner-memory.ts
@@ -11,6 +11,7 @@ import { runWithModelFallback } from "../../agents/model-fallback.js";
 import { listLegacyRuntimeModelProviderAliases } from "../../agents/model-runtime-aliases.js";
 import { isCliProvider } from "../../agents/model-selection.js";
 import { resolveContextConfigProviderForRuntime } from "../../agents/openai-codex-routing.js";
+import { resolveCompactionReserveTokensFloor } from "../../agents/pi-settings.js";
 import { resolveSandboxConfigForAgent, resolveSandboxRuntimeStatus } from "../../agents/sandbox.js";
 import {
   derivePromptTokens,
@@ -631,8 +632,7 @@ export async function runPreflightCompactionIfNeeded(params: {
   const memoryFlushPlan = resolveMemoryFlushPlan({ cfg: params.cfg, agentId: activeAgentId });
   const reserveTokensFloor =
     memoryFlushPlan?.reserveTokensFloor ??
-    resolveAgentCompactionConfig(params.cfg, activeAgentId)?.reserveTokensFloor ??
-    20_000;
+    resolveCompactionReserveTokensFloor(params.cfg, activeAgentId);
   const softThresholdTokens = memoryFlushPlan?.softThresholdTokens ?? 4_000;
   const freshPersistedTokens = resolveFreshSessionTotalTokens(entry);
   const persistedTotalTokens = entry.totalTokens;

openclaw/openclaw

src/auto-reply/reply/agent-runner-memory.test.ts
@@ -95,11 +95,11 @@ type EmbeddedAgentParams = {
 };
 
 type CompactEmbeddedAgentSessionParams = {
-  agentId?: string;
   authProfileId?: string;
   contextTokenBudget?: number;
   sessionKey?: string;
   sandboxSessionKey?: string;
+  agentId?: string;
   currentTokenCount?: number;
   cwd?: string;
   sessionFile?: string;
@@ -1063,6 +1063,54 @@ describe("runMemoryFlushIfNeeded", () => {
     expect(compactCall.authProfileId).toBe("anthropic:claude@martian.engineering");
     expect(compactCall.contextTokenBudget).toBe(258_000);
   });
+
+
+  it("uses inherited reserveTokensFloor for preflight projection with empty agent compaction", async () => {
+    clearMemoryPluginState();
+    const sessionEntry: SessionEntry = {
+      sessionId: "session",
+      updatedAt: Date.now(),
+      totalTokens: 5_000,
+      totalTokensFresh: false,
+    };
+
+    await runPreflightCompactionIfNeeded({
+      cfg: {
+        agents: {
+          defaults: {
+            model: "anthropic/claude-opus-4-6",
+            compaction: { reserveTokensFloor: 3_000 },
+          },
+          list: [
+            {
+              id: "lossless-agent",
+              model: { primary: "anthropic/claude-opus-4-6" },
+              compaction: {},
+            },
+          ],
+        },
+      },
+      followupRun: createTestFollowupRun({
+        agentId: "lossless-agent",
+        provider: "anthropic",
+        model: "claude-opus-4-6",
+        sessionKey: "agent:lossless-agent:main",
+      }),
+      defaultModel: "anthropic/claude-opus-4-6",
+      agentCfgContextTokens: 10_000,
+      sessionEntry,
+      sessionStore: { "agent:lossless-agent:main": sessionEntry },
+      sessionKey: "agent:lossless-agent:main",
+      isHeartbeat: false,
+      replyOperation: createReplyOperation(),
+    });
+
+    expect(compactEmbeddedAgentSessionMock).toHaveBeenCalledTimes(1);
+    const compactCall = requireCompactEmbeddedAgentSessionCall();
+    expect(compactCall.agentId).toBe("lossless-agent");
+    expect(compactCall.currentTokenCount).toBe(5_000);
+  });
+
   it("updates the active preflight run after transcript rotation", async () => {
     const sessionFile = path.join(rootDir, "session.jsonl");
     const successorFile = path.join(rootDir, "session-rotated.jsonl");

src/auto-reply/reply/agent-runner-memory.ts
@@ -1,13 +1,14 @@
 import crypto from "node:crypto";
 import fs from "node:fs";
 import path from "node:path";
-import { resolveAgentCompactionConfig, resolveAgentConfig } from "../../agents/agent-scope-config.js";
+import { resolveAgentConfig } from "../../agents/agent-scope-config.js";
 import { resolveBootstrapWarningSignaturesSeen } from "../../agents/bootstrap-budget.js";
 import { estimateMessagesTokens } from "../../agents/compaction.js";
 import {
   classifyCompactionReason,
   DEFERRED_CONTEXT_ENGINE_COMPACTION_REASON,
 } from "../../agents/embedded-agent-runner/compact-reasons.js";
+import { resolveCompactionReserveTokensFloor } from "../../agents/agent-settings.js";
 import { isRecoverableNativeHarnessBindingFailure } from "../../agents/harness/compaction-recovery.js";
 import { resolveAgentHarnessPolicy } from "../../agents/harness/policy.js";
 import { ensureSelectedAgentHarnessPlugin } from "../../agents/harness/runtime-plugin.js";
@@ -738,8 +739,7 @@ export async function runPreflightCompactionIfNeeded(params: {
   const memoryFlushPlan = resolveMemoryFlushPlan({ cfg: params.cfg, agentId: activeAgentId });
   const reserveTokensFloor =
     memoryFlushPlan?.reserveTokensFloor ??
-    resolveAgentCompactionConfig(params.cfg, activeAgentId)?.reserveTokensFloor ??
-    20_000;
+    resolveCompactionReserveTokensFloor(params.cfg, activeAgentId);
   const softThresholdTokens = memoryFlushPlan?.softThresholdTokens ?? 4_000;
   const freshPersistedTokens = resolveFreshSessionTotalTokens(entry);
   const persistedTotalTokens = entry.totalTokens;