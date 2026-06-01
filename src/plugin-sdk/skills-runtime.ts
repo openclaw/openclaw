auraecosystem/Openclaw-builder

fix(plugin-sdk): expose skills helper surfaces

src/plugin-sdk/skills-runtime.ts
@@ -5,3 +5,19 @@ export {
   shouldRefreshSnapshotForVersion,
   type SkillsChangeEvent,
 } from "../agents/skills/refresh-state.js";
+export {
+  parseFrontmatter,
+  resolveOpenClawMetadata,
+  resolveSkillKey,
+} from "../agents/skills/frontmatter.js";
+export {
+  loadVisibleWorkspaceSkillEntries,
+  loadWorkspaceSkillEntries,
+} from "../agents/skills/workspace.js";
+export type { Skill } from "../agents/skills/skill-contract.js";
+export type {
+  OpenClawSkillMetadata,
+  ParsedSkillFrontmatter,
+  SkillEligibilityContext,
+  SkillEntry,
+} from "../agents/skills/types.js";

openclaw/openclaw

src/plugin-sdk/skills-runtime.ts
@@ -5,3 +5,19 @@ export {
   shouldRefreshSnapshotForVersion,
   type SkillsChangeEvent,
 } from "../skills/runtime/refresh-state.js";
+export {
+  parseFrontmatter,
+  resolveOpenClawMetadata,
+  resolveSkillKey,
+} from "../skills/loading/frontmatter.js";
+export {
+  loadVisibleWorkspaceSkillEntries,
+  loadWorkspaceSkillEntries,
+} from "../skills/loading/workspace.js";
+export type { Skill } from "../skills/loading/skill-contract.js";
+export type {
+  OpenClawSkillMetadata,
+  ParsedSkillFrontmatter,
+  SkillEligibilityContext,
+  SkillEntry,
+} from "../skills/types.js";