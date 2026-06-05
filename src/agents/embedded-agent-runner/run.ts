auraecosystem/Openclaw-builder

fix(plugins): report selected failover target

src/agents/pi-embedded-runner/run.ts
@@ -1105,10 +1105,14 @@ export async function runEmbeddedPiAgent(
         nextAttemptPromptOverride = MID_TURN_PRECHECK_CONTINUATION_PROMPT;
         suppressNextUserMessagePersistence = true;
       };
+      const nextModelFallbackCandidate = params.nextModelFallbackCandidate;
       const maybeEscalateRateLimitProfileFallback = (params: {
         failoverProvider: string;
         failoverModel: string;
-        logFallbackDecision: (decision: "fallback_model", extra?: { status?: number }) => void;
+        logFallbackDecision: (
+          decision: "fallback_model",
+          extra?: { status?: number; targetProvider?: string; targetModel?: string },
+        ) => void;
       }) => {
         rateLimitProfileRotations += 1;
         if (rateLimitProfileRotations <= rateLimitProfileRotationLimit || !fallbackConfigured) {
@@ -1118,7 +1122,11 @@ export async function runEmbeddedPiAgent(
         log.warn(
           `rate-limit profile rotation cap reached for ${sanitizeForLog(provider)}/${sanitizeForLog(modelId)} after ${rateLimitProfileRotations} rotations; escalating to model fallback`,
         );
-        params.logFallbackDecision("fallback_model", { status });
+        params.logFallbackDecision("fallback_model", {
+          status,
+          targetProvider: nextModelFallbackCandidate?.provider,
+          targetModel: nextModelFallbackCandidate?.model,
+        });
         throw new FailoverError(
           "The AI service is temporarily rate-limited. Please try again in a moment.",
           {
@@ -2404,7 +2412,11 @@ export async function runEmbeddedPiAgent(
                 stage: "prompt",
                 ...(typeof status === "number" ? { status } : {}),
               });
-              logPromptFailoverDecision("fallback_model", { status });
+              logPromptFailoverDecision("fallback_model", {
+                status,
+                targetProvider: nextModelFallbackCandidate?.provider,
+                targetModel: nextModelFallbackCandidate?.model,
+              });
               await maybeBackoffBeforeOverloadFailover(promptFailoverReason);
               throw (
                 normalizedPromptFailover ??
@@ -2566,6 +2578,7 @@ export async function runEmbeddedPiAgent(
             maybeEscalateRateLimitProfileFallback,
             maybeBackoffBeforeOverloadFailover,
             advanceAuthProfile: advanceAttemptAuthProfile,
+            nextFallbackCandidate: nextModelFallbackCandidate,
           });
           overloadProfileRotations = assistantFailoverOutcome.overloadProfileRotations;
           if (assistantFailoverOutcome.action === "retry") {

openclaw/openclaw

src/agents/embedded-agent-runner/run.ts
@@ -1260,10 +1260,14 @@ export async function runEmbeddedAgent(
         nextAttemptPromptOverride = MID_TURN_PRECHECK_CONTINUATION_PROMPT;
         suppressNextUserMessagePersistence = true;
       };
+      const nextModelFallbackCandidate = params.nextModelFallbackCandidate;
       const maybeEscalateRateLimitProfileFallback = (paramsLocal: {
         failoverProvider: string;
         failoverModel: string;
-        logFallbackDecision: (decision: "fallback_model", extra?: { status?: number }) => void;
+        logFallbackDecision: (
+          decision: "fallback_model",
+          extra?: { status?: number; targetProvider?: string; targetModel?: string },
+        ) => void;
       }) => {
         rateLimitProfileRotations += 1;
         if (rateLimitProfileRotations <= rateLimitProfileRotationLimit || !fallbackConfigured) {
@@ -1273,7 +1277,11 @@ export async function runEmbeddedAgent(
         log.warn(
           `rate-limit profile rotation cap reached for ${sanitizeForLog(provider)}/${sanitizeForLog(modelId)} after ${rateLimitProfileRotations} rotations; escalating to model fallback`,
         );
-        paramsLocal.logFallbackDecision("fallback_model", { status });
+        paramsLocal.logFallbackDecision("fallback_model", {
+          status,
+          targetProvider: nextModelFallbackCandidate?.provider,
+          targetModel: nextModelFallbackCandidate?.model,
+        });
         throw new FailoverError(
           "The AI service is temporarily rate-limited. Please try again in a moment.",
           {
@@ -2681,7 +2689,11 @@ export async function runEmbeddedAgent(
                 stage: "prompt",
                 ...(typeof status === "number" ? { status } : {}),
               });
-              logPromptFailoverDecision("fallback_model", { status });
+              logPromptFailoverDecision("fallback_model", {
+                status,
+                targetProvider: nextModelFallbackCandidate?.provider,
+                targetModel: nextModelFallbackCandidate?.model,
+              });
               await maybeBackoffBeforeOverloadFailover(promptFailoverReason);
               throw (
                 normalizedPromptFailover ??
@@ -2855,6 +2867,7 @@ export async function runEmbeddedAgent(
             maybeEscalateRateLimitProfileFallback,
             maybeBackoffBeforeOverloadFailover,
             advanceAuthProfile: advanceAttemptAuthProfile,
+            nextFallbackCandidate: nextModelFallbackCandidate,
           });
           overloadProfileRotations = assistantFailoverOutcome.overloadProfileRotations;
           if (assistantFailoverOutcome.action === "retry") {