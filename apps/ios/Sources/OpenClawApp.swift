c1c372410db0ccf2157f5bed744a707ffc5851c2
Sash Zats <sash@zats.io>
2026-06-10 16:58:54 +0200

    feat(ios): show OpenClaw logo in notch

--- apps/ios/Sources/OpenClawApp.swift
+++ apps/ios/Sources/OpenClawApp.swift
@@ -652,6 +652,9 @@ struct OpenClawApp: App {
                     self.gatewayController.setScenePhase(newValue)
                     self.appDelegate.scenePhaseChanged(newValue)
                     self.applyAppearancePreference()
+                    if newValue == .active {
+                        OpenClawNotchPresenter.showIfNeeded()
+                    }
                 }
         }
     }
