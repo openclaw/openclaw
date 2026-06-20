07d338a660c27a0e7d84c6e0f33258bed4348df4
Shakker <shakkerdroid@gmail.com>
2026-06-20 04:04:49 +0100

    refactor: move route updates into lit adapter

--- ui/src/ui/app.ts
+++ ui/src/ui/app.ts
@@ -1035,6 +1035,7 @@ export class OpenClawApp extends LitElement {
       hash: "",
     };
     const routeState = appRouter.getState();
+    const previousRouteId = this.routeId;
     const activeMatch = routeState.matches[0];
     const revalidate = routeState.status === "success" && activeMatch?.routeId === next;
     const browserLocation = typeof window === "undefined" ? null : window.location;
@@ -1042,6 +1043,8 @@ export class OpenClawApp extends LitElement {
       (browserLocation?.pathname ?? routeState.resolvedLocation?.pathname) === location.pathname &&
       (browserLocation?.search ?? routeState.resolvedLocation?.search) === location.search &&
       (browserLocation?.hash ?? routeState.resolvedLocation?.hash) === location.hash;
+    this.routeId = next;
+    this.requestUpdate();
     void appRouter
       .navigate(
         next,
@@ -1052,7 +1055,16 @@ export class OpenClawApp extends LitElement {
         },
         location,
       )
-      .catch(() => undefined);
+      .then(
+        () => {
+          this.routeId = next;
+          this.requestUpdate();
+        },
+        () => {
+          this.routeId = previousRouteId;
+          this.requestUpdate();
+        },
+      );
     if (next !== "chat") {
       this.setChatMobileControlsOpen(false);
     }
