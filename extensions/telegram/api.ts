2e40e2cf2446708dec0b60ac6248f815b53cb95d
Peter Steinberger <steipete@gmail.com>
2026-07-06 14:55:01 -0400

    style(telegram): format public API exports

--- extensions/telegram/api.ts
+++ extensions/telegram/api.ts
@@ -1,8 +1,5 @@
 // Telegram API module exposes the plugin public contract.
-export type {
-  Message as TelegramBotMessage,
-  Update as TelegramBotUpdate,
-} from "grammy/types";
+export type { Message as TelegramBotMessage, Update as TelegramBotUpdate } from "grammy/types";
 export { telegramPlugin } from "./src/channel.js";
 export { telegramSetupPlugin } from "./src/channel.setup.js";
 export {
