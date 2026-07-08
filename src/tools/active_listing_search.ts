84b57777f6785e7f2945dccfb5630585fb047a1f
Tyffoni <tyffoni@gmail.com>
2026-07-07 18:20:11 -0700

    create property filterer

---
+++ src/tools/active_listing_search.ts
@@ -0,0 +1,50 @@
+export async function searchActiveListings(filters: PropertyFilters, page = 1, limit = 10) {
+  const offset = (page - 1) * limit;
+  let sql = `
+    SELECT
+        L_ListingID, L_DisplayId, L_Address, L_City, L_Zip,
+        L_SystemPrice AS price, L_Keyword2 AS beds, LM_Dec_3 AS baths,
+        LM_Int2_3 AS sqft, L_Type_ AS type, L_Status AS status,
+        LMD_MP_Latitude AS lat, LMD_MP_Longitude AS lng,
+        YearBuilt, AssociationFee, DaysOnMarket,
+        PoolPrivateYN, ViewYN, FireplaceYN, PhotoCount,
+        LA1_UserFirstName, LA1_UserLastName, LO1_OrganizationName
+        FROM rets_property WHERE L_Status = "Active"
+    `;
+  const params: any[] = [];
+  if (filters.city) {
+    sql += " AND L_City = ?";
+    params.push(filters.city);
+  }
+  if (filters.maxPrice) {
+    sql += " AND L_SystemPrice <= ?";
+    params.push(filters.maxPrice);
+  }
+  if (filters.beds) {
+    sql += " AND L_Keyword2 >= ?";
+    params.push(filters.beds);
+  }
+  if (filters.baths) {
+    sql += " AND LM_Dec_3 >= ?";
+    params.push(filters.baths);
+  }
+  if (filters.sqft) {
+    sql += " AND LM_Int2_3 >= ?";
+    params.push(filters.sqft);
+  }
+  if (filters.type) {
+    sql += " AND L_Type_ = ?";
+    params.push(filters.type);
+  }
+  if (filters.pool) {
+    sql += " AND PoolPrivateYN = ?";
+    params.push(filters.pool);
+  }
+  if (filters.hasView) {
+    sql += " AND ViewYN = ?";
+    params.push(filters.hasView);
+  }
+  sql += " ORDER BY L_SystemPrice ASC LIMIT ? OFFSET ?";
+  params.push(limit, offset);
+  return query<ListingRow>(sql, params);
+}
