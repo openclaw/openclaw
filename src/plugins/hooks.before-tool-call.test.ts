arbabkhan007/Open-Claw

fix(plugins): bound before_tool_call hook with a default timeout
before_tool_call gates every tool call (block / requireApproval) but was the only
modifying hook without a default entry in DEFAULT_MODIFYING_HOOK_TIMEOUT_MS_BY_HOOK.
A hung handler left the tool call awaited forever; the only backstop is the
run-level lane idle watchdog at the agent timeout (up to the 48h default), which
does not cancel the stuck hook, wedging that session's turn (lane concurrency 1)
far longer than any sibling modifying hook and leaking the abandoned promise.

before_tool_call is already registered fail-closed (hook-runner-global.ts) and
runBeforeToolCallHook converts a thrown hook error into a denied tool call, so a
per-hook timeout is safe and non-regressive: on timeout the tool is denied, never
silently allowed. Add before_tool_call: 15_000 to bring it in line with its
sibling modifying hooks.

src/plugins/hooks.before-tool-call.test.ts
@@ -1,7 +1,7 @@
 /** Tests before-tool-call hook ordering, mutation, and cancellation behavior. */
-import { beforeEach, describe, expect, it } from "vitest";
+import { beforeEach, describe, expect, it, vi } from "vitest";
 import { createHookRunner } from "./hooks.js";
-import { addStaticTestHooks } from "./hooks.test-helpers.js";
+import { addStaticTestHooks, createMockPluginRegistry } from "./hooks.test-helpers.js";
 import { createEmptyPluginRegistry, type PluginRegistry } from "./registry.js";
 import type { PluginHookToolContext } from "./types.js";
 import type { PluginHookBeforeToolCallResult } from "./types.js";
@@ -252,5 +252,31 @@ describe("before_tool_call hook merger — requireApproval", () => {
   ] as const)("$name", async ({ hooks, expected }) => {
     const result = await runBeforeToolCallWithHooks(registry, hooks);
     expectRequireApprovalResult(result, expected);
+  });
+});
+
+describe("before_tool_call hook runner — timeout", () => {
+  it("times out a hung handler and, being fail-closed, rejects so the tool call is denied", async () => {
+    vi.useFakeTimers();
+    try {
+      const handler = vi.fn(() => new Promise(() => {}));
+      const logger = { error: vi.fn(), warn: vi.fn(), debug: vi.fn() };
+      const runner = createHookRunner(
+        createMockPluginRegistry([{ hookName: "before_tool_call", handler }]),
+        // Matches the global runner (hook-runner-global.ts): before_tool_call is
+        // fail-closed, so a timed-out handler throws instead of silently allowing.
+        { logger, failurePolicyByHook: { before_tool_call: "fail-closed" } },
+      );
+
+      const run = runner.runBeforeToolCall({ toolName: "bash", params: {} }, stubCtx);
+      const rejection = expect(run).rejects.toThrow(
+        "before_tool_call handler from test-plugin failed: timed out after 15000ms",
+      );
+
+      await vi.advanceTimersByTimeAsync(15_000);
+      await rejection;
+    } finally {
+      vi.useRealTimers();
+    }
   });
 });

openclaw/openclaw

src/plugins/hooks.before-tool-call.test.ts
@@ -1,7 +1,7 @@
 /** Tests before-tool-call hook ordering, mutation, and cancellation behavior. */
-import { beforeEach, describe, expect, it } from "vitest";
+import { beforeEach, describe, expect, it, vi } from "vitest";
 import { createHookRunner } from "./hooks.js";
-import { addStaticTestHooks } from "./hooks.test-fixtures.js";
+import { addStaticTestHooks, createMockPluginRegistry } from "./hooks.test-fixtures.js";
 import { createEmptyPluginRegistry, type PluginRegistry } from "./registry.js";
 import type { PluginHookToolContext } from "./types.js";
 import type { PluginHookBeforeToolCallResult } from "./types.js";
@@ -252,5 +252,31 @@ describe("before_tool_call hook merger — requireApproval", () => {
   ] as const)("$name", async ({ hooks, expected }) => {
     const result = await runBeforeToolCallWithHooks(registry, hooks);
     expectRequireApprovalResult(result, expected);
+  });
+});
+
+describe("before_tool_call hook runner — timeout", () => {
+  it("times out a hung handler and, being fail-closed, rejects so the tool call is denied", async () => {
+    vi.useFakeTimers();
+    try {
+      const handler = vi.fn(() => new Promise(() => {}));
+      const logger = { error: vi.fn(), warn: vi.fn(), debug: vi.fn() };
+      const runner = createHookRunner(
+        createMockPluginRegistry([{ hookName: "before_tool_call", handler }]),
+        // Matches the global runner (hook-runner-global.ts): before_tool_call is
+        // fail-closed, so a timed-out handler throws instead of silently allowing.
+        { logger, failurePolicyByHook: { before_tool_call: "fail-closed" } },
+      );
+
+      const run = runner.runBeforeToolCall({ toolName: "bash", params: {} }, stubCtx);
+      const rejection = expect(run).rejects.toThrow(
+        "before_tool_call handler from test-plugin failed: timed out after 15000ms",
+      );
+
+      await vi.advanceTimersByTimeAsync(15_000);
+      await rejection;
+    } finally {
+      vi.useRealTimers();
+    }
   });
 });