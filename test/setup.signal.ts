5505650b428f853525fb081eaeea9b85b903807b
Ayaan Zaidi <hi@obviy.us>
2026-09-12 20:27:03 +0530

    test(signal): parse manifest before creating metadata fixture

--- test/setup.signal.ts
+++ test/setup.signal.ts
@@ -1,7 +1,14 @@
+import { fileURLToPath } from "node:url";
 import { afterEach, beforeEach } from "vitest";
-import manifest from "../extensions/signal/openclaw.plugin.json" with { type: "json" };
 import { setCurrentPluginMetadataSnapshot } from "../src/plugins/current-plugin-metadata.test-support.js";
+import { loadPluginManifest } from "../src/plugins/manifest.js";
 import { createPluginMetadataSnapshotFixture } from "../src/plugins/plugin-metadata.test-support.js";
+
+const loaded = loadPluginManifest(fileURLToPath(new URL("../extensions/signal", import.meta.url)));
+if (!loaded.ok) {
+  throw new Error(loaded.error);
+}
+const manifest = loaded.manifest;
 
 beforeEach(() => {
   setCurrentPluginMetadataSnapshot(createPluginMetadataSnapshotFixture({ plugins: [manifest] }));
